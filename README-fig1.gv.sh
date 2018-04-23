a4897fcec8dd2cb2cdc49aad9dfdff2eafbd4fbf
B. van Berkum <dev@dotmpe.com>
2018-04-23 16:30:16 +0200

    Minor SVG style change

--- README-fig1.gv.sh
+++ README-fig1.gv.sh
@@ -1,10 +1,11 @@
 digraph {
   rankdir="LR" ;
   node [ shape=Mrecord ] ;
+  stylesheet="style.svg.css" ;
 
-  GIT [ label="GIT", URL="$package_urls_source_code__0" ] ;
-  Travis [ label="Travis", URL="$package_urls_builds__0" ] ;
-  Docker_Hub [ label="Hub", URL="$package_urls_images__0" ] ;
+  GIT [ label="GIT", URL="$package_urls_source_code__0", style=filled, fillcolor=transparent ] ;
+  Travis [ label="Travis", URL="$package_urls_builds__0", style=filled, fillcolor=transparent ] ;
+  Docker_Hub [ label="Hub", URL="$package_urls_images__0", style=filled, fillcolor=transparent ] ;
 
   GIT -> Travis [ label="Tag or commit" ]
   Travis -> Docker_Hub [ label="Build, Tag & Push" ] ;
