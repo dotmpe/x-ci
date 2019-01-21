6b4c98077b25a240fe761eee15996aaf61dcf2c5
B. van Berkum <berend@brixcrm.nl>
2019-01-21 22:54:56 +0100

    Adding realpath to CI deps

---
+++ tools/ci/script.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+uname -s && echo Term $TERM && echo Shell $SHELL
+cat /etc/os-release
+
+case "$(uname -s)" in
+    Linux|Darwin ) DOCKER_IMAGE=centurylink/golang-builder-cross ;;
+    MSYS_NT-10.0) DOCKER_IMAGE=FIXME # golang:windowsservercore-1803
+  ;;
+esac
+
+docker run --rm \
+  -e CGO_ENABLED=true \
+  -e COMPRESS_BINARY=true \
+  -e BUILD_GOOS="$BUILD_GOOS" \
+  -e BUILD_GOARCH="$BUILD_GOARCH" \
+  -v "$(pwd -P):/src" \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  $DOCKER_IMAGE $APP_ID:local
+
+# Sync: U-S:
